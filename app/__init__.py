d187969adb46777900a80dd6dcdc7d9041445ded
imosudi <imosudi@gmail.com>
2020-11-01 09:25:09 +0100

    initial configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap(app)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
