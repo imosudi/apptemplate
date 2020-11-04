8b152431a6c9913170108dc1829fc65f9a12601f
imosudi <imosudi@gmail.com>
2020-11-04 16:23:13 +0100

    App configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
 from flask_bootstrap import Bootstrap
+
+from flask_script import Manager
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+
+
+moment = Moment(app)
+
 
 bootstrap = Bootstrap(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from flask_script import Manager
+from flask_moment import Moment
+
+from datetime import datetime
+
+
+
 
 from app import app
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
