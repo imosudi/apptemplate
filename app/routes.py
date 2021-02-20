cfd5866008b13fb195c91f10d9bbdb3b72a2464c
imosudi <imosudi@gmail.com>
2021-02-20 17:26:02 +0100

    app update -  Login & Registration Forms

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,26 @@ from datetime import datetime
 
 from app import app
 
+from app.models import *
+
 @app.route('/')
 def home():
     return render_template('index.html', current_time=datetime.utcnow())
+    
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pageName = 'login'
+    form = loginForm(request.form)
+    return render_template('login.html', pageName=pageName, form=form, current_time=datetime.utcnow())
+	
+
+
+#User registration
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    pageName = 'register'
+    form = registrationForm(request.form)
+    return render_template('register.html', pageName=pageName, form=form, current_time=datetime.utcnow())
+
